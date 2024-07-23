051d16bec1d09e533b7b4b77ef69424376b9202d
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2024-07-23 20:12:01 +0800

    feat: product_cart.php
    - translate product_cart
    - update livewire payment method
    - set lang .env

---
+++ resources/lang/id/product_cart.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'product' => 'Produk',
+    'net_unit_price' => 'Harga Satuan Bersih',
+    'stock' => 'Stok',
+    'quantity' => 'Jumlah',
+    'discount' => 'Diskon',
+    'tax' => 'Pajak',
+    'sub_total' => 'Sub Total',
+    'action' => 'Aksi',
+    'shipping' => 'Pengiriman',
+    'grand_total' => 'Total Keseluruhan',
+    'search_select_products' => 'Silakan cari & pilih produk',
+    'payment_method' => 'Metode Pembayaran',
+];
