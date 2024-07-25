cbbf869327262f46948bda7dacaf3b64d2d411e0
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2024-07-25 18:32:32 +0800

    feat: add indonesian lang to profile page

---
+++ resources/lang/id/profile.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'intro' => 'Halo',
+    'sub_header' => 'Ubah informasi profil & kata sandi Anda dari sini...',
+    'user_profile' => 'Profile Pengguna',
+    'photo_profile' => 'Foto Profile',
+    'name' => 'Nama',
+    'current_pass' => 'Password Saat Ini',
+    'new_pass' => 'Password Baru',
+    'conf_pass' => 'Konfirmasi Password',
+];
