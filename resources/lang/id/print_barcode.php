061348523ab21c0e96517563cdc4e71312280f2c
rfargo <renaldy_fargo@yahoo.com>
2024-07-28 17:16:38 +0800

    feat: translate barcode

--- resources/lang/id/print_barcode.php
+++ resources/lang/id/print_barcode.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
-    'home' => 'Home',
+    'title' => 'Cetak Barcode',
+    'note' => 'CATATAN: Kode Produk harus berupa angka untuk menghasilkan barcode!',
+    'product_name' => 'Nama Product',
+    'code' => 'Kode',
+    'quantity' => 'Kuantitas',
+    'search_select_products' => 'Silakan cari & pilih produk!',
+    'generate_barcode' => 'Buat Barcode',
+    'loading' => 'Loading...',
+    'download_pdf' => 'Download PDF',
+    'price' => 'Harga',
 ];
