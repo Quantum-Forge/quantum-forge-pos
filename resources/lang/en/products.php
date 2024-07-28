d1e68765f92a9b1854a3fd096d6f06dac15e4a50
rfargo <renaldy_fargo@yahoo.com>
2024-07-28 17:32:06 +0800

    feat: translate product index

---
+++ resources/lang/en/products.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => 'Products',
+    'add_product' => 'Add Product',
+    'image' => 'Image',
+    'category' => 'Category',
+    'code' => 'Code',
+    'name' => 'Name',
+    'cost' => 'Cost',
+    'price' => 'Price',
+    'quantity' => 'Quantity',
+];
---
+++ resources/lang/id/products.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => 'Produk',
+    'add_product' => 'Tambah Produk',
+    'image' => 'Gambar',
+    'category' => 'Kategori',
+    'code' => 'Kode',
+    'name' => 'Nama',
+    'cost' => 'Biaya',
+    'price' => 'Harga',
+    'quantity' => 'Kuantitas',
+];
