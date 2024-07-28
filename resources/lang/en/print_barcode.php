f5037a3aace4880494b26c16edc4e577cabc5df0
rfargo <renaldy_fargo@yahoo.com>
2024-07-28 17:16:38 +0800

    feat: translate barcode

--- resources/lang/en/print_barcode.php
+++ resources/lang/en/print_barcode.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
-    'home' => 'Home',
+    'title' => 'Print Barcode',
+    'note' => 'NOTE: Product Code must be a number to generate barcodes!',
+    'product_name' => 'Product Name',
+    'code' => 'Code',
+    'quantity' => 'Quantity',
+    'search_select_products' => 'Please search & select a product!',
+    'generate_barcode' => 'Generate Barcode',
+    'loading' => 'Loading...',
+    'download_pdf' => 'Download PDF',
+    'price' => 'Price',
 ];
--- resources/views/livewire/barcode/product-table.blade.php
+++ resources/views/livewire/barcode/product-table.blade.php
@@ -14,34 +14,37 @@
             <div class="table-responsive-md">
                 <table class="table table-bordered mb-0">
                     <thead>
-                    <tr class="align-middle">
-                        <th class="align-middle">Product Name</th>
-                        <th class="align-middle">Code</th>
-                        <th class="align-middle">
-                            Quantity <i class="bi bi-question-circle-fill text-info" data-toggle="tooltip" data-placement="top" title="Max Quantity: 100"></i>
-                        </th>
-                    </tr>
+                        <tr class="align-middle">
+                            <th class="align-middle">{{ __('print_barcode.product_name') }}</th>
+                            <th class="align-middle">{{ __('print_barcode.code') }}</th>
+                            <th class="align-middle">
+                                {{ __('print_barcode.quantity') }} <i class="bi bi-question-circle-fill text-info"
+                                    data-toggle="tooltip" data-placement="top" title="Max Quantity: 100"></i>
+                            </th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        @if(!empty($product))
-                            <td class="align-middle">{{ $product->product_name }}</td>
-                            <td class="align-middle">{{ $product->product_code }}</td>
-                            <td class="align-middle text-center" style="width: 200px;">
-                                <input wire:model.live="quantity" class="form-control" type="number" min="1" max="100" value="{{ $quantity }}">
-                            </td>
-                        @else
-                            <td colspan="3" class="text-center">
-                                <span class="text-danger">Please search & select a product!</span>
-                            </td>
-                        @endif
-                    </tr>
+                        <tr>
+                            @if (!empty($product))
+                                <td class="align-middle">{{ $product->product_name }}</td>
+                                <td class="align-middle">{{ $product->product_code }}</td>
+                                <td class="align-middle text-center" style="width: 200px;">
+                                    <input wire:model.live="quantity" class="form-control" type="number" min="1"
+                                        max="100" value="{{ $quantity }}">
+                                </td>
+                            @else
+                                <td colspan="3" class="text-center">
+                                    <span class="text-danger">{{ __('print_barcode.search_select_products') }}</span>
+                                </td>
+                            @endif
+                        </tr>
                     </tbody>
                 </table>
             </div>
             <div class="mt-3">
-                <button wire:click="generateBarcodes({{ $product }}, {{ $quantity }})" type="button" class="btn btn-primary">
-                    <i class="bi bi-upc-scan"></i> Generate Barcodes
+                <button wire:click="generateBarcodes({{ $product }}, {{ $quantity }})" type="button"
+                    class="btn btn-primary">
+                    <i class="bi bi-upc-scan"></i> {{ __('print_barcode.generate_barcode') }}
                 </button>
             </div>
         </div>
@@ -50,23 +53,26 @@
     <div wire:loading wire:target="generateBarcodes" class="w-100">
         <div class="d-flex justify-content-center">
             <div class="spinner-border text-primary" role="status">
-                <span class="sr-only">Loading...</span>
+                <span class="sr-only">{{ __('print_barcode.loading') }}</span>
             </div>
         </div>
     </div>
 
-    @if(!empty($barcodes))
+    @if (!empty($barcodes))
         <div class="text-right mb-3">
             <button wire:click="getPdf" wire:loading.attr="disabled" type="button" class="btn btn-primary">
-                <span wire:loading wire:target="getPdf" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                <i wire:loading.remove wire:target="getPdf" class="bi bi-file-earmark-pdf"></i> Download PDF
+                <span wire:loading wire:target="getPdf" class="spinner-border spinner-border-sm" role="status"
+                    aria-hidden="true"></span>
+                <i wire:loading.remove wire:target="getPdf" class="bi bi-file-earmark-pdf"></i>
+                {{ __('print_barcode.download_pdf') }}
             </button>
         </div>
         <div class="card">
             <div class="card-body">
                 <div class="row justify-content-center">
-                    @foreach($barcodes as $barcode)
-                        <div class="col-lg-3 col-md-4 col-sm-6" style="border: 1px solid #ffffff;border-style: dashed;background-color: #48FCFE;">
+                    @foreach ($barcodes as $barcode)
+                        <div class="col-lg-3 col-md-4 col-sm-6"
+                            style="border: 1px solid #ffffff;border-style: dashed;background-color: #48FCFE;">
                             <p class="mt-3 mb-1" style="font-size: 15px;color: #000;">
                                 {{ $product->product_name }}
                             </p>
@@ -74,7 +80,7 @@
                                 {!! $barcode !!}
                             </div>
                             <p style="font-size: 15px;color: #000;">
-                                Price:: {{ format_currency($product->product_price) }}
+                                {{ __('print_barcode.price') }}: {{ format_currency($product->product_price) }}
                             </p>
                         </div>
                     @endforeach
