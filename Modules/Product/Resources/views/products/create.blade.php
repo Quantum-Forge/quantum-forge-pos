7b1f84f462f762368e6d81e8a1cbdac42e0e89dd
rfargo <renaldy_fargo@yahoo.com>
2024-07-28 17:52:10 +0800

    feat: translate create product

--- Modules/Product/Resources/views/products/create.blade.php
+++ Modules/Product/Resources/views/products/create.blade.php
@@ -5,8 +5,8 @@
 @section('breadcrumb')
     <ol class="breadcrumb border-0 m-0">
         <li class="breadcrumb-item"><a href="{{ route('home') }}">{{ __('sidebar.home') }}</a></li>
-        <li class="breadcrumb-item"><a href="{{ route('products.index') }}">Products</a></li>
-        <li class="breadcrumb-item active">Add</li>
+        <li class="breadcrumb-item"><a href="{{ route('products.index') }}">{{ __('products.title') }}</a></li>
+        <li class="breadcrumb-item active">{{ __('products.create.short_title') }}</li>
     </ol>
 @endsection
 
@@ -18,7 +18,8 @@
                 <div class="col-lg-12">
                     @include('utils.alerts')
                     <div class="form-group">
-                        <button class="btn btn-primary">Create Product <i class="bi bi-check"></i></button>
+                        <button class="btn btn-primary">{{ __('products.create.title') }} <i
+                                class="bi bi-check"></i></button>
                     </div>
                 </div>
                 <div class="col-lg-12">
@@ -27,14 +28,16 @@
                             <div class="form-row">
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="product_name">Product Name <span class="text-danger">*</span></label>
+                                        <label for="product_name">{{ __('products.create.product_name') }} <span
+                                                class="text-danger">*</span></label>
                                         <input type="text" class="form-control" name="product_name" required
                                             value="{{ old('product_name') }}">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="product_code">Code <span class="text-danger">*</span></label>
+                                        <label for="product_code">{{ __('products.create.code') }} <span
+                                                class="text-danger">*</span></label>
                                         <input type="text" class="form-control" name="product_code" required
                                             value="{{ old('product_code') }}">
                                     </div>
@@ -43,29 +46,33 @@
 
                             <div class="form-row">
                                 <div class="col-md-6">
-                                    <label for="category_id">Category <span class="text-danger">*</span></label>
+                                    <label for="category_id">{{ __('products.create.category') }} <span
+                                            class="text-danger">*</span></label>
                                     <div class="input-group">
                                         <select class="form-control" name="category_id" id="category_id" required>
-                                            <option value="" selected disabled>Select Category</option>
+                                            <option value="" selected disabled>
+                                                {{ __('products.create.select_category') }}</option>
                                             @foreach (\Modules\Product\Entities\Category::all() as $category)
-                                                <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                                                <option value="{{ $category->id }}">{{ $category->category_name }}
+                                                </option>
                                             @endforeach
                                         </select>
                                         <div class="input-group-append d-flex">
                                             <button data-toggle="modal" data-target="#categoryCreateModal"
                                                 class="btn btn-outline-primary" type="button">
-                                                Add
+                                                {{ __('products.create.add_category') }}
                                             </button>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="barcode_symbology">Barcode Symbology <span
+                                        <label for="barcode_symbology">{{ __('products.create.barcode_symbology') }} <span
                                                 class="text-danger">*</span></label>
                                         <select class="form-control" name="product_barcode_symbology" id="barcode_symbology"
                                             required>
-                                            <option value="" selected disabled>Select Symbology</option>
+                                            <option value="" selected disabled>
+                                                {{ __('products.create.select_symbology') }}</option>
                                             <option value="C128">Code 128</option>
                                             <option value="C39">Code 39</option>
                                             <option value="UPCA">UPC-A</option>
@@ -80,14 +87,16 @@
                             <div class="form-row">
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="product_cost">Cost <span class="text-danger">*</span></label>
+                                        <label for="product_cost">{{ __('products.create.cost') }} <span
+                                                class="text-danger">*</span></label>
                                         <input id="product_cost" type="text" class="form-control" name="product_cost"
                                             required value="{{ old('product_cost') }}">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="product_price">Price <span class="text-danger">*</span></label>
+                                        <label for="product_price">{{ __('products.create.price') }} <span
+                                                class="text-danger">*</span></label>
                                         <input id="product_price" type="text" class="form-control" name="product_price"
                                             required value="{{ old('product_price') }}">
                                     </div>
@@ -97,14 +106,15 @@
                             <div class="form-row">
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="product_quantity">Quantity <span class="text-danger">*</span></label>
+                                        <label for="product_quantity">{{ __('products.create.quantity') }} <span
+                                                class="text-danger">*</span></label>
                                         <input type="number" class="form-control" name="product_quantity" required
                                             value="{{ old('product_quantity') }}" min="1">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="product_stock_alert">Alert Quantity <span
+                                        <label for="product_stock_alert">{{ __('products.create.alert_quantity') }} <span
                                                 class="text-danger">*</span></label>
                                         <input type="number" class="form-control" name="product_stock_alert" required
                                             value="{{ old('product_stock_alert', 0) }}" min="0" max="100">
@@ -115,29 +125,32 @@
                             <div class="form-row">
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <label for="product_order_tax">Tax (%)</label>
+                                        <label for="product_order_tax">{{ __('products.create.tax') }}
+                                            (%)</label>
                                         <input type="number" class="form-control" name="product_order_tax"
                                             value="{{ old('product_order_tax') }}" min="1">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <label for="product_tax_type">Tax type</label>
+                                        <label for="product_tax_type">{{ __('products.create.tax_type') }}</label>
                                         <select class="form-control" name="product_tax_type" id="product_tax_type">
-                                            <option value="" selected>Select Tax Type</option>
-                                            <option value="1">Exclude PPN</option>
-                                            <option value="2">Include PPN</option>
+                                            <option value="" selected>{{ __('products.create.select_tax_type') }}
+                                            </option>
+                                            <option value="1">{{ __('products.create.exclude_ppn') }}</option>
+                                            <option value="2">{{ __('products.create.include_ppn') }}</option>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <label for="product_unit">Unit <i class="bi bi-question-circle-fill text-info"
-                                                data-toggle="tooltip" data-placement="top"
-                                                title="This short text will be placed after Product Quantity."></i> <span
-                                                class="text-danger">*</span></label>
+                                        <label for="product_unit">{{ __('products.create.unit') }} <i
+                                                class="bi bi-question-circle-fill text-info" data-toggle="tooltip"
+                                                data-placement="top" title="{{ __('products.create.qty_text') }}"></i>
+                                            <span class="text-danger">*</span></label>
                                         <select class="form-control" name="product_unit" id="product_unit">
-                                            <option value="" selected>Select Unit</option>
+                                            <option value="" selected>{{ __('products.create.select_unit') }}
+                                            </option>
                                             @foreach (\Modules\Setting\Entities\Unit::all() as $unit)
                                                 <option value="{{ $unit->short_name }}">
                                                     {{ $unit->name . ' | ' . $unit->short_name }}</option>
@@ -148,7 +161,7 @@
                             </div>
 
                             <div class="form-group">
-                                <label for="product_note">Note</label>
+                                <label for="product_note">{{ __('products.create.note') }}</label>
                                 <textarea name="product_note" id="product_note" rows="4 " class="form-control"></textarea>
                             </div>
                         </div>
@@ -159,8 +172,9 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="form-group">
-                                <label for="image">Product Images <i class="bi bi-question-circle-fill text-info"
-                                        data-toggle="tooltip" data-placement="top"
+                                <label for="image">{{ __('products.create.product_images') }} <i
+                                        class="bi bi-question-circle-fill text-info" data-toggle="tooltip"
+                                        data-placement="top"
                                         title="Max Files: 3, Max File Size: 1MB, Image Size: 400x400"></i></label>
                                 <div class="dropzone d-flex flex-wrap align-items-center justify-content-center"
                                     id="document-dropzone">
@@ -240,13 +254,13 @@
             $('#product_cost').maskMoney({
                 prefix: '{{ settings()->currency->symbol }}',
                 thousands: '{{ settings()->currency->thousand_separator }}',
-                decimal:'{{ settings()->currency->decimal_separator }}',
+                decimal: '{{ settings()->currency->decimal_separator }}',
                 precision: 0,
             });
             $('#product_price').maskMoney({
                 prefix: '{{ settings()->currency->symbol }}',
                 thousands: '{{ settings()->currency->thousand_separator }}',
-                decimal:'{{ settings()->currency->decimal_separator }}',
+                decimal: '{{ settings()->currency->decimal_separator }}',
                 precision: 0,
             });
 
