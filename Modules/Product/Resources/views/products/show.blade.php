a2a3eeb28f6268827ed760093dba2cc45b8feb53
rfargo <renaldy_fargo@yahoo.com>
2024-07-28 18:55:30 +0800

    feat: product section translate

--- Modules/Product/Resources/views/products/show.blade.php
+++ Modules/Product/Resources/views/products/show.blade.php
@@ -5,8 +5,8 @@
 @section('breadcrumb')
     <ol class="breadcrumb border-0 m-0">
         <li class="breadcrumb-item"><a href="{{ route('home') }}">{{ __('sidebar.home') }}</a></li>
-        <li class="breadcrumb-item"><a href="{{ route('products.index') }}">Products</a></li>
-        <li class="breadcrumb-item active">Details</li>
+        <li class="breadcrumb-item"><a href="{{ route('products.index') }}">{{ __('products.title') }}</a></li>
+        <li class="breadcrumb-item active">{{ __('products.show.details') }}</li>
     </ol>
 @endsection
 
@@ -14,7 +14,12 @@
     <div class="container-fluid mb-4">
         <div class="row mb-3">
             <div class="col-md-12">
-                {!! \Milon\Barcode\Facades\DNS1DFacade::getBarCodeSVG($product->product_code, $product->product_barcode_symbology, 2, 110) !!}
+                {!! \Milon\Barcode\Facades\DNS1DFacade::getBarCodeSVG(
+                    $product->product_code,
+                    $product->product_barcode_symbology,
+                    2,
+                    110,
+                ) !!}
             </div>
         </div>
         <div class="row">
@@ -24,62 +29,64 @@
                         <div class="table-responsive">
                             <table class="table table-bordered table-striped mb-0">
                                 <tr>
-                                    <th>Product Code</th>
+                                    <th>{{ __('products.show.product_code') }}</th>
                                     <td>{{ $product->product_code }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Barcode Symbology</th>
+                                    <th>{{ __('products.show.barcode_symbology') }}</th>
                                     <td>{{ $product->product_barcode_symbology }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Name</th>
+                                    <th>{{ __('products.show.name') }}</th>
                                     <td>{{ $product->product_name }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Category</th>
+                                    <th>{{ __('products.show.category') }}</th>
                                     <td>{{ $product->category->category_name }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Cost</th>
+                                    <th>{{ __('products.show.cost') }}</th>
                                     <td>{{ format_currency($product->product_cost) }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Price</th>
+                                    <th>{{ __('products.show.price') }}</th>
                                     <td>{{ format_currency($product->product_price) }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Quantity</th>
+                                    <th>{{ __('products.show.quantity') }}</th>
                                     <td>{{ $product->product_quantity . ' ' . $product->product_unit }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Stock Worth</th>
+                                    <th>{{ __('products.show.stock_worth') }}</th>
                                     <td>
-                                        COST:: {{ format_currency($product->product_cost * $product->product_quantity) }} /
-                                        PRICE:: {{ format_currency($product->product_price * $product->product_quantity) }}
+                                        {{ Str::upper(__('products.show.cost')) }}::
+                                        {{ format_currency($product->product_cost * $product->product_quantity) }} /
+                                        {{ Str::upper(__('products.show.price')) }}::
+                                        {{ format_currency($product->product_price * $product->product_quantity) }}
                                     </td>
                                 </tr>
                                 <tr>
-                                    <th>Alert Quantity</th>
+                                    <th>{{ __('products.show.alert_quantity') }}</th>
                                     <td>{{ $product->product_stock_alert }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Tax (%)</th>
+                                    <th>{{ __('products.show.tax') }} (%)</th>
                                     <td>{{ $product->product_order_tax ?? 'N/A' }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Tax Type</th>
+                                    <th>{{ __('products.show.tax_type') }}</th>
                                     <td>
-                                        @if($product->product_tax_type == 1)
-                                            Exclude PPN
+                                        @if ($product->product_tax_type == 1)
+                                            {{ __('products.show.exclude_ppn') }}
                                         @elseif($product->product_tax_type == 2)
-                                            Include PPN
+                                            {{ __('products.show.include_ppn') }}
                                         @else
                                             N/A
                                         @endif
                                     </td>
                                 </tr>
                                 <tr>
-                                    <th>Note</th>
+                                    <th>{{ __('products.show.note') }}</th>
                                     <td>{{ $product->product_note ?? 'N/A' }}</td>
                                 </tr>
                             </table>
@@ -94,7 +101,8 @@
                         @forelse($product->getMedia('images') as $media)
                             <img src="{{ $media->getUrl() }}" alt="Product Image" class="img-fluid img-thumbnail mb-2">
                         @empty
-                            <img src="{{ $product->getFirstMediaUrl('images') }}" alt="Product Image" class="img-fluid img-thumbnail mb-2">
+                            <img src="{{ $product->getFirstMediaUrl('images') }}" alt="Product Image"
+                                class="img-fluid img-thumbnail mb-2">
                         @endforelse
                     </div>
                 </div>
@@ -102,6 +110,3 @@
         </div>
     </div>
 @endsection
-
-
-
