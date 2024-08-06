c73867c8fd4179257c67d824f7f88193e80ecd11
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2024-08-06 19:18:42 +0800

    feat: translate profit-loss-report

---
+++ resources/lang/en/reports.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'profit_loss_report' => [
+        'sales' => 'Sales',
+        'sale_returns' => 'Sale Returns',
+        'profit' => 'Profit',
+        'purchases' => 'Purchases',
+        'purchase_returns' => 'Purchase Returns',
+        'expenses' => 'Expenses',
+        'payments_sent' => 'Payments Sent',
+        'payments_received' => 'Payments Received',
+        'payments_net' => 'Payments Net',
+        'start_date' => 'Start Date',
+        'end_date' => 'End Date',
+        'filter_report' => 'Filter Report'
+    ],
+   
+    
+];
---
+++ resources/lang/id/reports.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'profit_loss_report' => [
+        'sales' => 'Penjualan',
+        'sale_returns' => 'Retur Penjualan',
+        'profit' => 'Keuntungan',
+        'purchases' => 'Pembelian',
+        'purchase_returns' => 'Retur Pembelian',
+        'expenses' => 'Pengeluaran',
+        'payments_sent' => 'Pembayaran Terkirim',
+        'payments_received' => 'Pembayaran Diterima',
+        'payments_net' => 'Pembayaran Bersih',
+        'start_date' => 'Tanggal Mulai',
+        'end_date' => 'Tanggal Berakhir',
+        'filter_report' => 'Filter Laporan'
+    ],
+   
+    
+];
